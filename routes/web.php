f23639bef0bd059291e63ab439fb59e6d57feef9
ekpono <ekponoambrose@gmail.com>
2018-09-12 23:12:05 +0100

    fixes

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,7 @@ Route::get('/', function () {
     return view('index');
 });
 
-<<<<<<< HEAD
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/blog', function () {
-    return view('blog');
-});
-
-=======
 // Route::post('/pay', 'PaymentController@drop')->name('pay');
->>>>>>> a070b72e42afc4f11a5012a1bc73ee52a4b08303
 Route::post('/pay', [
     'uses' => 'PaymentController@redirectToGateway',
     'as' => 'pay'
